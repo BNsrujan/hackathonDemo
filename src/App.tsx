3b2d9ed5cadb90f68015c7326af966202c28b790
BNsrujan <srujanbn123@gmail.com>
2024-11-14 20:29:20 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Card from "./components/Card"
+
+function App() {
+
+  return (
+    <section>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Card />
+    </section>
+  )
+}
+
+export default App
---
+++ src/components/Card.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button.tsx"
+
+
+function Card() {
+    return (
+        <div>
+            <Button>Click me</Button>
+        </div>
+    )
+}
+
+export default Card
